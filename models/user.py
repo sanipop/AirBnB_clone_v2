80ee23f28ce1920cb064b4a224db326d6eac5d38
sanipop <67932273+sanipop@users.noreply.github.com>
2024-02-20 15:06:57 +0100

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""The scripts for the User class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+
+
+class User(BaseModel, Base):
+    """User class inherits BaseModel and Base
+    Attributes:
+        email: the email for the user
+        password: the users password 
+        first_name: the users first name
+        last_name: And the last name of user
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
