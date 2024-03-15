a2344710396b94e4252748dcce6c3376b7b6a003
sanipop <67932273+sanipop@users.noreply.github.com>
2024-03-15 04:45:21 +0100

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,25 @@
-from fabric import Connection, task
+from fabric import task
 from datetime import datetime
 import os
 
-hosts = ["100.26.18.247", "54.144.158.59"]
-
-
-def do_pack():
+@task
+def do_pack(c):
     """method to compress directory web_static."""
     dt = datetime.utcnow()
     file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
     if not os.path.isdir("versions"):
         os.makedirs("versions")
-    result = local("tar -cvzf {} web_static".format(file_name))
+    result = c.local("tar -cvzf {} web_static".format(file_name))
     if result.failed:
         return None
     return file_name
 
-
-def do_deploy(archive_path):
+@task
+def do_deploy(c, archive_path):
     """Deploy to web server.
 
     Args:
+        c: Fabric Connection object.
         archive_path (str): directory to dist.
     Returns:
         no file or error - False.
@@ -31,7 +30,6 @@ def do_deploy(archive_path):
     file_name = os.path.basename(archive_path)
     name = os.path.splitext(file_name)[0]
 
-    c = Connection('ubuntu@100.26.18.247')
     c.put(archive_path, "/tmp/{}".format(file_name))
     c.run("rm -rf /data/web_static/releases/{}/".format(name))
     c.run("mkdir -p /data/web_static/releases/{}/".format(name))
@@ -43,15 +41,10 @@ def do_deploy(archive_path):
     c.run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
     return True
 
-
 @task
 def deploy(c):
     """deploy web static."""
-    file = do_pack()
+    file = do_pack(c)
     if file is None:
         return False
     return do_deploy(c, file)
-
-
-if __name__ == "__main__":
-    deploy()
